fa1b0d81f49c83f4559c7a56f9d521f0940cfe58
Gao Yun <gao_yun@qq.com>
2016-09-19 03:46:56 +0000

    add logo link_to home

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,18 +7,23 @@ class SearchController < ApplicationController
   	url= params[:search]
 
   	if url != nil
-  	djkk_url = URI.escape(url)
-  	doc = Nokogiri::HTML(open(djkk_url))
+  		begin
+	  	djkk_url = URI.escape(url)
+	  	doc = Nokogiri::HTML(open(djkk_url))
+	  	#response=Net::HTTP.get_response(URI(djkk_url))
+	  	inline_script = doc.xpath('//script[not(@src)]')
+	  	@download_url=inline_script.first.text.delete( ";" "]" "+")[20..1000].gsub(/title|artist|m4a: s_str/, 'title'=>'"title"', 'artist'=>'"artist"', 'm4a: s_str'=>'"m4a":')
+	  	@http_str = "http://mx.djkk.com/mix"
+	  	@title=JSON.parse(@download_url)['title']
+	  	@artist=JSON.parse(@download_url)['artist']
+	  	@m4a=@http_str+JSON.parse(@download_url)['m4a']
 
-  	#response=Net::HTTP.get_response(URI(djkk_url))
-  	inline_script = doc.xpath('//script[not(@src)]')
-  	@download_url=inline_script.first.text.delete( ";" "]" "+")[20..1000].gsub(/title|artist|m4a: s_str/, 'title'=>'"title"', 'artist'=>'"artist"', 'm4a: s_str'=>'"m4a":')
-  	@http_str = "http://mx.djkk.com/mix"
-  	@title=JSON.parse(@download_url)['title']
-  	@artist=JSON.parse(@download_url)['artist']
-  	@m4a=@http_str+JSON.parse(@download_url)['m4a']
-  	  #JSON.parse(response.body)['trans_result'][0]['dst']
-	#@download_url=JSON.parse(@download_url)
+ 
+	  	rescue Exception => e #如果上面的代码执行发生异常就捕获  
+	  	    
+	  	   flash.now[:notice] = "请输入正确的地址！"
+	  	end  
+  
   	end
   end
 end
